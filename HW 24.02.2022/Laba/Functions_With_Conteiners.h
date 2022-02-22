55d42d5b7f615cf4641112f8f056aa383b989a8e
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2022-02-22 13:22:32 +0300

    changed to class

--- HW 24.02.2022/Laba/Functions_With_Conteiners.h
+++ HW 24.02.2022/Laba/Functions_With_Conteiners.h
@@ -10,6 +10,17 @@
 #include "numeric"
 #include "list"
 
+class My_Str {
+private:
+    std::string Str_;
+public:
+    My_Str(std::string Str) : Str_(Str) {};
+
+    bool operator == (My_Str Str_El) {
+        return Str_El.Str_ == Str_;
+    }
+};
+
 void Fill_Vector_With_Numbers(std::ifstream &Input_File, std::vector<int> &Vector_For_Input);
 
 void Print_Vector(std::vector<int> &Vector_For_Input);
@@ -20,9 +31,9 @@ int CountIF_Num_Same(std::vector<int> &Vector_For_Input, int For_Search);
 
 int Find_Average(std::vector<int> &Vector_For_Input);
 
-int Count_Equal_Strings(std::list<std::string> &List_For_Input, std::string For_Search);
+int Count_Equal_Strings(std::list<My_Str> &List_For_Input, My_Str Str_El);
 
-int CountIF_Equal_Strings(std::list<std::string> &List_For_Input, std::string For_Search);
+int CountIF_Equal_Strings(std::list<My_Str> &List_For_Input, My_Str Str_El);
 
 
 #endif //LABA_FUNCTIONS_WITH_CONTEINERS_H
--- HW 24.02.2022/Laba/main.cpp
+++ HW 24.02.2022/Laba/main.cpp
@@ -2,15 +2,32 @@
 #include "Functions_With_Conteiners.h"
 
 int main() {
-    std::ifstream file("First_Input.txt");
-    std::list<std::string> First_list;
-    Fill_List_With_Text(file, First_list);
-    Print_List(First_list);
+//    std::ifstream file("First_Input.txt");
+//    std::list<std::string> First_list;
+//    Fill_List_With_Text(file, First_list);
+//    Print_List(First_list);
+//
+//    std::ifstream Numbers("Second_Input.txt");
+//    std::vector<int> First_vector;
+//    Fill_Vector_With_Numbers(Numbers, First_vector);
+//    Print_Vector(First_vector);
 
-    std::ifstream Numbers("Second_Input.txt");
-    std::vector<int> First_vector;
-    Fill_Vector_With_Numbers(Numbers, First_vector);
-    Print_Vector(First_vector);
+    std::string str1 = "aaaa";
+    My_Str Str1(str1);
+    std::string str2 = "bbb";
+    My_Str Str2(str2);
+    std::string str3 = "11111";
+    My_Str Str3(str3);
+    std::string str4 = "bdadad";
+    My_Str Str4(str4);
+    std::string str5 = "bdadad";
+    My_Str Str5(str5);
+    std::list<My_Str> My_list = {Str1,Str2,Str3,Str4,Str5};
+
+    std::string str6 = "bdadad";
+    My_Str Str6(str6);
+
+    Count_Equal_Strings(My_list,Str6);
 
     return 0;
 }
