2fd2b05fc0f38e05cd75c0da34620bc195e28b26
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2022-04-12 21:33:57 +0300

    tetst

--- HW 14.04.2022/HW140422/Module.asm
+++ HW 14.04.2022/HW140422/Module.asm
@@ -4,7 +4,12 @@
 
 
 .model flat
-.data
+.const
+
+	A_n real4 5.0
+	B_n real4 12.0
+	C_n real4 13.0
+
 .code
 
 task_1 proc C, x:QWORD, y:QWORD
@@ -24,9 +29,53 @@ finit
 	fld y
 	fmul st(0), st(0)
 	fmul st(0), st(1); cot*y^2 --> st(0)
-	fadd st(0), st(3)
+	fadd st(0), st(4)
 ret
 task_1 endp
+
+
+
+task_3 proc C, epsilon:QWORD, fir_board:QWORD, sec_board:QWORD
+
+finit
+
+fld epsilon
+
+ret
+task_3 endp
+
+
+
+
+Eq_form proc C, x:QWORD 
+
+finit
+fld x
+fmul st(0),st(0)
+fld x
+fmul st(0), st(1) ; x^3
+fld x
+fmul st(0), st(0)
+fmul A_n ; A*x^2
+fadd st(0), st(1) ; A*x^2 + x^3 - st(0)
+fld x
+fmul B_n
+fadd st(0), st(1) ; B*x + A*x^2 + x^3 - st(0)
+fadd C_n ; C + B*x + A*x^2 + x^3 - st(0)
+
+ret
+Eq_form endp
+
+
+task_2 proc C, epsilon:QWORD, fir_board:QWORD, sec_board:QWORD
+
+fld fir_board
+call Eq_form
+
+
+ret
+task_2 endp
+
 
 
 
--- HW 14.04.2022/HW140422/Source.cpp
+++ HW 14.04.2022/HW140422/Source.cpp
@@ -5,35 +5,38 @@
 extern "C"
 {
 	double __cdecl task_1(double, double);
+	double __cdecl task_2(double, double, double);
+	double __cdecl task_3(double);
 }
 
+double get_PI()
+{
+	double pi;
+	__asm
+	{
+		fldpi
+		fstp pi
+	}
+	return pi;
+}
 
 int main() {
 	double x = 4.055;
 	double y = 11.755;
 
-	std::cout << "z = " << std::setprecision(20) << task_1(x, y) << "\n";
-	std::cout << "cpp check : " << std::setprecision(20) << ((x * x) * (tan(x / y))) + ((y * y) * (cos(x/y) / sin(x / y)));
+	std::cout << "z = " << std::setprecision(20) << task_1(x, y) << std::endl;
+	std::cout << "cpp check : " << std::setprecision(20) << ((x * x) * (tan(x / y))) + ((y * y) * (cos(x/y) / sin(x / y))) <<  std::endl;
+
+
+	double epsilon = 0.0000000123;
+	double pi = get_PI()/12;
+
+	std::cout << "pi check : " << std::setprecision(20) << pi << std::endl;
+	std::cout << "PI = " << std::setprecision(20) << task_3(epsilon) << std::endl; 
+
+
+	double f_b = -12.515;
+	double s_b = 1.755;
+	std::cout << "Root = " << std::setprecision(20) << task_2(epsilon, f_b , s_b) << std::endl;
 
 }
-
-
-//fdiv st(0), st(1)
-//fptan; tan - st(1)
-//
-//fld x; tg - 2
-//fmul st(0), st(0); tg - 1
-//fmul st(0), st(2); x ^ 2 * tg - 0
-//
-//fld y
-//fld x; x ^ 2 * tg - 2
-//
-//fdiv st(0), st(1)
-//fptan
-//fdiv st(0), st(1); cot(x / y) - 0
-//
-//fld y; cot(x / y) - 1
-//fmul st(0), st(0)
-//fmul st(0), st(1); y ^ 2 * cot - 0
-//
-//fadd st(0), st(3)
